7db3e323e6487a226b0663fc5933ef37d2b44712
matyias <tougepl@gmail.com>
2017-05-02 15:14:15 +0200

    hotfix

--- main.py
+++ main.py
@@ -14,11 +14,10 @@ class GUI(Gtk.Window):
     def __init__(self):
         # Gtk.Window.__init__(self, default_width=600, default_height=450, title="XKCD Browser")
         Gtk.Window.__init__(self, title="XKCD Browser")
-        self.tytul = Gtk.Label("Hello World!")
-        self.numer = Gtk.Label("4321")
-        self.btn_najnowszy = Gtk.Button(label="Pokaż najnowszy")
-        self.btn_nastepny = Gtk.Button(label="Następny")
-        self.btn_poprzedni = Gtk.Button(label="Poprzedni")
+        self.tytul = Gtk.Label()
+        self.btn_najnowszy = Gtk.Button(label=">|")
+        self.btn_nastepny = Gtk.Button(label=">")
+        self.btn_poprzedni = Gtk.Button(label="<")
         self.entry_nr_komiksu = Gtk.Entry()
         self.obrazek = Gtk.Image()
         self.vbox1 = Gtk.VBox(spacing=20)
@@ -26,11 +25,11 @@ class GUI(Gtk.Window):
         self.hbox2 = Gtk.HBox()
         self.hbox3 = Gtk.HBox()
 
+        self.btn_poprzedni.set_hint("Poprzedni")
         self.entry_nr_komiksu.set_size_request(10, 30)
         self.hbox1.set_baseline_position(1)
 
         # Pakowanie
-        self.vbox1.pack_start(self.numer, False, True, 0)
         self.vbox1.pack_start(self.tytul, False, True, 0)
         self.vbox1.pack_start(self.obrazek, False, True, 0)
         self.hbox1.pack_start(self.btn_poprzedni, False, False, 0)
@@ -43,33 +42,36 @@ class GUI(Gtk.Window):
         self.add(self.vbox1)
 
         self.set_border_width(20)
-        self.entry_nr_komiksu.connect('activate', self.ustaw_obrazek)
         self.connect("delete-event", Gtk.main_quit) 
         self.show_all()
-
-    def ustaw_obrazek(self, obrazek):
-        self.obrazek.set_from_pixbuf(GdkPixbuf.Pixbuf.new_from_file(obrazek))
 
 
 class App:
     def __init__(self):
         self.gui = GUI()
-        self.pobierz_komiks("http://xkcd.com")
-        self.gui.ustaw_obrazek("cache/" + self.komiks_info("http://xkcd.com")["nazwa_img"])
+        self.xkcd_addr = "http://xkcd.com"
         self.gui.entry_nr_komiksu.connect("activate", self.wczytaj_komiks)
+        self.gui.btn_najnowszy.connect("clicked", self.wczytaj_komiks)
 
-    def komiks_info(self, addr):
-        return {"url": "http:" + BeautifulSoup(urllib.urlopen(addr), "html.parser").find_all(id="comic")[0].img["src"],
-                "nazwa_komiksu": BeautifulSoup(urllib.urlopen(addr), "html.parser").find_all(id="ctitle")[0].text,
-                "nazwa_img": self.url_komiksu(addr).split('/')[-1]}
+    def komiks_info(self, adres):
+        soup = BeautifulSoup(urllib.urlopen(adres), "html.parser")
+        return {"url": "http:" + soup.find_all(id="comic")[0].img["src"],
+                "nazwa_komiksu": soup.find_all(id="ctitle")[0].text,
+                "nazwa_img": soup.find_all(id="comic")[0].img["src"].split('/')[-1]}
 
-    def wczytaj_komiks(self, entry):
-        addr = "http://xkcd.com/" + entry.get_text()
-        self.pobierz_komiks(addr)
-        self.gui.ustaw_obrazek("cache/" + self.komiks_info(addr)["nazwa_img"])
-        print self.komiks_info(addr)["nazwa_komiksu"]
+    def pobierz_komiks(self, adres):
+        if not os.path.exists("cache"):
+            os.mkdir("cache")
+        
+        if not os.path.exists("cache/" + self.komiks_info(adres)["nazwa_img"]):
+            urllib.urlretrieve(self.komiks_info(adres)["url"], "cache/" + self.komiks_info(adres)["nazwa_img"])
 
-    
+    def wczytaj_komiks(self, wejscie):
+        adres = "http://xkcd.com/" + wejscie.get_text()
+        self.gui.tytul.set_markup("Komiks {}: ".format(wejscie.get_text()) + "<b>{}</b>".format(self.komiks_info(adres)["nazwa_komiksu"]))
+        self.pobierz_komiks(adres)
+        self.gui.obrazek.set_from_pixbuf(GdkPixbuf.Pixbuf.new_from_file("cache/" + self.komiks_info(adres)["nazwa_img"]))
+ 
 def main():
     app = App()
     Gtk.main()
@@ -77,3 +79,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
